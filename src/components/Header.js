c61c85ad002ea1c1ae31214cb346de3915865839
Meena024 <maill2rmeena@gmail.com>
2024-07-26 12:20:59 +0530

    Header creation

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Card from "./UI/Card";
+
+const Header = () => {
+  return (
+    <Card>
+      <h1>Blog Website</h1>
+      <button>Add New Blog</button>
+    </Card>
+  );
+};
+
+export default Header;

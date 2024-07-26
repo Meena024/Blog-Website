327df350a7c6c6bd797f9ca4fede90be2ee71e8b
Meena024 <maill2rmeena@gmail.com>
2024-07-26 14:15:23 +0530

    Blogs posted are listed and delete function executed

--- src/components/BlogsListed.js
+++ src/components/BlogsListed.js
@@ -1,31 +1,24 @@
 import Card from "./UI/Card";
+import Context from "../Store/Context";
 import classes from "./BlogsListed.module.css";
+import { useContext } from "react";
 
 const BlogsListed = () => {
-  const blogList = [
-    {
-      id: "blog1",
-      Url: "https://t3.ftcdn.net/jpg/03/26/50/30/360_F_326503028_qCpvm89l0xwuP3EibsQTb1nUL5O8Eavu.jpg",
-      title: "Peacock Feather",
-      description: "hello description1",
-    },
-    {
-      id: "blog2",
-      Url: "https://img.pikbest.com/wp/202344/fluffy-pastel-colored-bird-feathers-abstract-background-with-a-textured-twist_9926209.jpg!sw800",
-      title: "Feather",
-      description: "hello description",
-    },
-  ];
+  const { BlogsList, setBlogsList } = useContext(Context);
+
+  const deleteHandler = (id) => {
+    setBlogsList((prevBlogs) => prevBlogs.filter((blog) => blog.id !== id));
+  };
 
   return (
     <Card>
-      {blogList.map((blog) => (
+      {BlogsList.map((blog) => (
         <div key={blog.id} className={classes.blog}>
           <h2>{blog.title}</h2>
           <img src={blog.Url} />
           <p>{blog.description}</p>
           <button>Edit</button>
-          <button>Delete</button>
+          <button onClick={() => deleteHandler(blog.id)}>Delete</button>
         </div>
       ))}
     </Card>
